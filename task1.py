b43e642f583cf15ac74104be328af65404c14990
Jayarsalvani <jayarsalvaniofficials@gmail.com>
2024-10-03 11:46:29 +0800

    mytask1

---
+++ task1.py
@@ -0,0 +1,6 @@
+num=int(input("favfruits"))
+
+num=input("favfruits")
+ 
+favfruits=[]
+favfruits.append(num)
---
+++ task2.py
@@ -0,0 +1,16 @@
+#Task 1
+# Create a program that will ask the user how many favorite fruits they would like to insert
+# And then append those fruits to yout array, then print those names of fruits
+
+validfruits=["banana","apple","orange","mango"]
+for G in validfruits:
+    fruits=input("enter fruits:")
+    if fruits == "banana":
+        print ("hellow banana")
+    elif fruits == "apple":
+        print ("hellow apple")
+    elif fruits == "orange":
+        print ("no")
+    elif fruits == "mango":
+        print ("no")
+    else: print ("bye")
